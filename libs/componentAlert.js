104d3c7615b421608c71bf7c1971a6f6d8cf2e82
DiegoRojas98 <94511497+DiegoRojas98@users.noreply.github.com>
2022-12-15 12:51:37 -0500

    Update componentAlert.js

--- libs/componentAlert.js
+++ libs/componentAlert.js
@@ -1,6 +1,6 @@
 /* Requiere jquery => 3 */
-let componetZ = '<div style="z-index: 10;display: none;opacity: 0.95;width: 300px;min-height: 50px;height:auto;padding: 20px;border-radius: 15px;overflow: hidden;background-color: #d4edda;position:fixed;top: 20px;left:79%" id="boxAlert"></div>';
-$('body').prepend(`${componetZ}`);
+let component = '<div style="z-index: 10;display: none;opacity: 0.95;width: 300px;min-height: 50px;height:auto;padding: 20px;border-radius: 15px;overflow: hidden;background-color: #d4edda;position:fixed;top: 20px;left:79%" id="boxAlert"></div>';
+$('body').prepend(`${component}`);
 
 function boxAlert(text,type =  'null',seconds = 7){
     var time = seconds;
